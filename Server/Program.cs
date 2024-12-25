26259e3f4e69c63a2da8e4c8758015448b26f425
BenApplegate <applegateben03@gmail.com>
2024-12-24 23:42:11 -0700

    Updated entry point to be a class rather than top level statements

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("This is a test");
+﻿// Author: Benjamin Applegate
+// Creation: 24/12/2024 22:36 PM MST
+// Last Updated: 24/12/2024
+
+//This file serves as the main entrypoint to the server,
+//it sets up logging, and setting up other initial systems essential for running the server
+
+namespace Server;
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        
+    }
+}
